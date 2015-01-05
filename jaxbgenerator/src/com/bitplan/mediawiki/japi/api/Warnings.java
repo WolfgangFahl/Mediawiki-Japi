8d8c43471f09571a38dc00fc288a174042b8be57
Wolfgang Fahl <wf@bitplan.com>
2015-01-05 17:21:21 +0100

    adds new files

---
+++ jaxbgenerator/src/com/bitplan/mediawiki/japi/api/Warnings.java
@@ -0,0 +1,76 @@
+//
+// Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.8-b130911.1802 generiert 
+// Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
+// Generiert: 2015.01.05 um 03:45:04 PM CET 
+//
+
+
+package com.bitplan.mediawiki.japi.api;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java-Klasse für anonymous complex type.
+ * 
+ * <p>Das folgende Schemafragment gibt den erwarteten Content an, der in dieser Klasse enthalten ist.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="tokens">
+ *           &lt;complexType>
+ *             &lt;simpleContent>
+ *               &lt;extension base="&lt;http://www.w3.org/2001/XMLSchema>string">
+ *               &lt;/extension>
+ *             &lt;/simpleContent>
+ *           &lt;/complexType>
+ *         &lt;/element>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "tokens"
+})
+public class Warnings {
+
+    @XmlElement(required = true)
+    protected Tokens tokens;
+
+    /**
+     * Ruft den Wert der tokens-Eigenschaft ab.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Tokens }
+     *     
+     */
+    public Tokens getTokens() {
+        return tokens;
+    }
+
+    /**
+     * Legt den Wert der tokens-Eigenschaft fest.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Tokens }
+     *     
+     */
+    public void setTokens(Tokens value) {
+        this.tokens = value;
+    }
+
+}
