99c19b3d8aa04a80b93b872da962591587a556b8
Wolfgang Fahl <wf@bitplan.com>
2015-01-25 12:53:35 +0100

    fixes to and fromJson

--- src/main/java/com/bitplan/mediawiki/japi/jaxb/JaxbFactory.java
+++ src/main/java/com/bitplan/mediawiki/japi/jaxb/JaxbFactory.java
@@ -32,121 +32,144 @@ import org.eclipse.persistence.jaxb.MarshallerProperties;
  * @param <T>
  */
 public class JaxbFactory<T> implements JaxbFactoryApi<T> {
-	final Class<T> classOfT;
-	
-	protected static java.util.logging.Logger LOGGER = java.util.logging.Logger
+  final Class<T> classOfT;
+
+  protected static java.util.logging.Logger LOGGER = java.util.logging.Logger
       .getLogger("com.bitplan.mediawiki.japi.jaxb");
-	
-	/**
-	 * construct me for the given T class - workaround for java generics type
-	 * erasure
-	 * 
-	 * @param pClassOfT
-	 */
-	public JaxbFactory(Class<T> pClassOfT) {
-		classOfT = pClassOfT;
-	}
 
-	/**
-	 * get an instance of T for the given xml string
-	 * 
-	 * @param xml
-	 *          - the xml representation of the <T> instance
-	 * @return T
-	 * @throws Exception 
-	 */
-	@SuppressWarnings("unchecked")
-	public T fromXML(String xml) throws Exception {
-		// unmarshal the xml message to an Mediawiki Api Java object
-		JAXBContext context = JAXBContext.newInstance(classOfT);
-		Unmarshaller u = context.createUnmarshaller();
-		StringReader xmlReader = new StringReader(xml);
-		T result = null;
-		// this step will convert from xml text to Java Object
-		try {
-			Object unmarshalResult = u.unmarshal(xmlReader);
-			if (classOfT.isInstance(unmarshalResult)) {
-				result = (T) unmarshalResult;
-			}
-		} catch (JAXBException jex) {
-			String msg="JAXBException: "+jex.getMessage();
-			LOGGER.log(Level.SEVERE,msg);
-			LOGGER.log(Level.SEVERE,xml);
-			throw (new Exception(msg,jex));
-		}
-		return result;
-	}
+  /**
+   * construct me for the given T class - workaround for java generics type
+   * erasure
+   * 
+   * @param pClassOfT
+   */
+  public JaxbFactory(Class<T> pClassOfT) {
+    classOfT = pClassOfT;
+  }
 
-	/**
-	 * get an instance of T for the given json string
-	 * 
-	 * @param json
-	 *          - the json representation of the <T> instance
-	 * @return T
-	 * @throws Exception 
-	 */
-	public T fromJson(String json) throws Exception {
-		return fromXML(json);
-	}
+  /**
+   * get a fitting Unmarshaller
+   * 
+   * @return - the Unmarshaller for the classOfT set
+   * @throws JAXBException
+   */
+  public Unmarshaller getUnmarshaller() throws JAXBException {
+    JAXBContext context = JAXBContext.newInstance(classOfT);
+    Unmarshaller u = context.createUnmarshaller();
+    return u;
+  }
 
-	/**
-	 * get a marshaller for the given <T> instance
-	 * 
-	 * @param instance
-	 *          - the instance to get a marshaller for
-	 * @return a marshaller for <T>
-	 * @throws JAXBException
-	 */
-	public Marshaller getMarshaller(T instance) throws JAXBException {
-		JAXBContext context = JAXBContext.newInstance(instance.getClass());
-		Marshaller marshaller = context.createMarshaller();
-		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-		return marshaller;
-	}
+  @SuppressWarnings("unchecked")
+  public T fromString(Unmarshaller u, String s) throws Exception {
+    // unmarshal the string to a Java object of type <T> (classOfT has the runtime type)
+    StringReader stringReader = new StringReader(s);
+    T result = null;
+    // this step will convert from xml text to Java Object
+    try {
+      Object unmarshalResult = u.unmarshal(stringReader);
+      if (classOfT.isInstance(unmarshalResult)) {
+        result = (T) unmarshalResult;
+      } else {
+        throw new Exception("unmarshalling returned "+unmarshalResult.getClass().getName()+" but "+classOfT.getName()+" was expected");
+      }
+    } catch (JAXBException jex) {
+      String msg = "JAXBException: " + jex.getMessage();
+      LOGGER.log(Level.SEVERE, msg);
+      LOGGER.log(Level.SEVERE, s);
+      throw (new Exception(msg, jex));
+    }
+    return result;
+  }
 
-	/**
-	 * get the string representation of the given marshaller
-	 * 
-	 * @param marshaller
-	 * @param instance 
-	 * @return the string representation for the given marshaller
-	 * @throws JAXBException
-	 */
-	public String getString(Marshaller marshaller, T instance) throws JAXBException {
-		StringWriter sw = new StringWriter();
-		marshaller.marshal(instance, sw);
-		String result = sw.toString();
-		return result;
-	}
+  /**
+   * get an instance of T for the given xml string
+   * 
+   * @param xml
+   *          - the xml representation of the <T> instance
+   * @return T
+   * @throws Exception
+   */
+  public T fromXML(String xml) throws Exception {
+    Unmarshaller u=this.getUnmarshaller();
+    u.setProperty(MarshallerProperties.MEDIA_TYPE, "application/xml");
+    T result=this.fromString(u, xml);
+    return result;
+  }
 
-	/**
-	 * create a Json representation for the given <T> instance
-	 * 
-	 * @param instance
-	 *          - the instance to convert to json
-	 * @return a Json representation of the given <T>
-	 * @throws JAXBException
-	 */
-	public String asJson(T instance) throws JAXBException {
-		Marshaller marshaller = getMarshaller(instance);
-		marshaller.setProperty(MarshallerProperties.MEDIA_TYPE, "application/json");
-		marshaller.setProperty(MarshallerProperties.JSON_INCLUDE_ROOT, false);
-		String result = getString(marshaller,instance);
-		return result;
-	}
+  /**
+   * get an instance of T for the given json string
+   * 
+   * @param json
+   *          - the json representation of the <T> instance
+   * @return T
+   * @throws Exception
+   */
+  public T fromJson(String json) throws Exception {
+    Unmarshaller u=this.getUnmarshaller();
+    u.setProperty(MarshallerProperties.MEDIA_TYPE, "application/json");
+    T result=this.fromString(u, json);
+    return result;
+  }
 
-	/**
-	 * create an xml representation for the given <T> instance
-	 * 
-	 * @param instance
-	 *          - the instance to convert to xml
-	 * @return a xml representation of the given <T> instance
-	 * @throws JAXBException
-	 */
-	public String asXml(T instance) throws JAXBException {
-		Marshaller marshaller = getMarshaller(instance);
-		marshaller.setProperty(MarshallerProperties.MEDIA_TYPE, "application/xml");
-		String result = getString(marshaller,instance);
-		return result;
-	}
+  /**
+   * get a marshaller for the given <T> instance
+   * 
+   * @param instance
+   *          - the instance to get a marshaller for
+   * @return a marshaller for <T>
+   * @throws JAXBException
+   */
+  public Marshaller getMarshaller(T instance) throws JAXBException {
+    JAXBContext context = JAXBContext.newInstance(instance.getClass());
+    Marshaller marshaller = context.createMarshaller();
+    marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+    return marshaller;
+  }
+
+  /**
+   * get the string representation of the given marshaller
+   * 
+   * @param marshaller
+   * @param instance
+   * @return the string representation for the given marshaller
+   * @throws JAXBException
+   */
+  public String getString(Marshaller marshaller, T instance)
+      throws JAXBException {
+    StringWriter sw = new StringWriter();
+    marshaller.marshal(instance, sw);
+    String result = sw.toString();
+    return result;
+  }
+
+  /**
+   * create a Json representation for the given <T> instance
+   * 
+   * @param instance
+   *          - the instance to convert to json
+   * @return a Json representation of the given <T>
+   * @throws JAXBException
+   */
+  public String asJson(T instance) throws JAXBException {
+    Marshaller marshaller = getMarshaller(instance);
+    marshaller.setProperty(MarshallerProperties.MEDIA_TYPE, "application/json");
+    marshaller.setProperty(MarshallerProperties.JSON_INCLUDE_ROOT, true);
+    String result = getString(marshaller, instance);
+    return result;
+  }
+
+  /**
+   * create an xml representation for the given <T> instance
+   * 
+   * @param instance
+   *          - the instance to convert to xml
+   * @return a xml representation of the given <T> instance
+   * @throws JAXBException
+   */
+  public String asXml(T instance) throws JAXBException {
+    Marshaller marshaller = getMarshaller(instance);
+    marshaller.setProperty(MarshallerProperties.MEDIA_TYPE, "application/xml");
+    String result = getString(marshaller, instance);
+    return result;
+  }
 }
