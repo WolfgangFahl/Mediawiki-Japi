8af6928c39f17e14d940c61688eeae368b6a031e
Wolfgang Fahl <wf@bitplan.com>
2015-06-01 17:30:06 +0200

    switch off debugging for testGetApiDescription

--- src/test/java/com/bitplan/mediawiki/japi/TestApiDescription.java
+++ src/test/java/com/bitplan/mediawiki/japi/TestApiDescription.java
@@ -22,36 +22,39 @@ import com.bitplan.mediawiki.japi.api.Warnings;
 
 /**
  * test Api description access
+ * 
  * @author wf
  *
  */
 public class TestApiDescription extends APITestbase {
 
-	/**
-	 * 
-	 * @throws Exception
-	 */
-	@Test
-	public void testGetApiDescription() throws Exception {
-		// ExampleWiki lwiki=ewm.get("mediawiki-japi-test1_24");
-		// com.bitplan.mediawiki.japi.Mediawiki wiki=(Mediawiki) lwiki.wiki;
-		Mediawiki wiki=new Mediawiki("http://en.wikipedia.org");
-		wiki.setDebug(true);
-		// |phpfm|query+allpages|query+siteinfo
-		String params="&modules=main%7Clogin%7Clogout%7Cphpfm&helpformat=none";
-		Api result=wiki.getActionResult("paraminfo", params);
-		assertNotNull(result);
-		Warnings warnings = result.getWarnings();
-		assertNull(warnings);
-		Paraminfo paraminfo = result.getParaminfo();
-		assertNotNull(paraminfo);
-		List<Module> modules = paraminfo.getModules();
-		for (Module module:modules) {
-			System.out.println(module.getName());
-		}
-		assertEquals(4,modules.size());
-		
-	}
+  /**
+   * 
+   * @throws Exception
+   */
+  @Test
+  public void testGetApiDescription() throws Exception {
+    // ExampleWiki lwiki=ewm.get("mediawiki-japi-test1_24");
+    // com.bitplan.mediawiki.japi.Mediawiki wiki=(Mediawiki) lwiki.wiki;
+    Mediawiki wiki = new Mediawiki("http://en.wikipedia.org");
+    // wiki.setDebug(true);
+    // |phpfm|query+allpages|query+siteinfo
+    String params = "&modules=main%7Clogin%7Clogout%7Cphpfm&helpformat=none";
+    Api result = wiki.getActionResult("paraminfo", params);
+    assertNotNull(result);
+    Warnings warnings = result.getWarnings();
+    assertNull(warnings);
+    Paraminfo paraminfo = result.getParaminfo();
+    assertNotNull(paraminfo);
+    List<Module> modules = paraminfo.getModules();
+    if (debug) {
+      for (Module module : modules) {
+        System.out.println(module.getName());
+      }
+    }
+    assertEquals(4, modules.size());
 
-	// https://github.com/wiztools/xsd-gen
+  }
+
+  // https://github.com/wiztools/xsd-gen
 }
