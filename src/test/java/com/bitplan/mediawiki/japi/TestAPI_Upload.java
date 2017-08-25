075f84e06085f0fa932cfe3ca9f75955fcd8f3d2
Wolfgang Fahl <wf@bitplan.com>
2017-08-25 17:19:21 +0200

    checks existence of credentials

--- src/test/java/com/bitplan/mediawiki/japi/TestAPI_Upload.java
+++ src/test/java/com/bitplan/mediawiki/japi/TestAPI_Upload.java
@@ -36,43 +36,53 @@ import com.bitplan.mediawiki.japi.api.Ii;
  */
 public class TestAPI_Upload extends APITestbase {
 
-	/**
-	 * test uploading a file
-	 * 
-	 * @throws Exception
-	 */
-	@Test
-	public void testUpload() throws Exception {
-    ExampleWiki lwiki=ewm.get("mediawiki-japi-test1_24");
-    lwiki.login();
-    ClassLoader classLoader = getClass().getClassLoader();
-    File file = new File(classLoader.getResource("upload/Radcliffe_Chastenay_-_Les_Mysteres_d_Udolphe_frontispice_T6.jpg").getFile());
-    String filename=file.getName();
-    String contents="http://commons.wikimedia.org/wiki/File:Radcliffe_Chastenay_-_Les_Mysteres_d_Udolphe_frontispice_T6.jpg";
-    String reason="test upload "+lwiki.wiki.getIsoTimeStamp();
-    lwiki.wiki.upload(file, filename, contents, reason);
-    String targetUrl=lwiki.wiki.getImageInfo("File:"+filename).getUrl();
-    assertTrue("url '"+targetUrl+"' should exist",this.urlExists(targetUrl));
-	}
-	
-	/**
-	 * test uploading via image information
-	 * @throws Exception
-	 */
-	@Test
-	public void testUploadViaII() throws Exception {
-	  ExampleWiki sourceWiki = ewm.get("sourceWiki");
-	  // sourceWiki.wiki.setDebug(true);
-	  String imageName="Index.png";
-	  String imageTitle="File:"+imageName;
-    Ii ii=sourceWiki.getImageInfo(imageTitle);
-    ExampleWiki targetWiki =ewm.get("targetWiki");
-    String pageContent=sourceWiki.wiki.getPageContent(imageTitle);
-    targetWiki.login();
-    targetWiki.wiki.upload(ii, imageName, pageContent);
-    // targetWiki.wiki.setDebug(true);
-    String targetUrl=targetWiki.wiki.getImageInfo(imageTitle).getUrl();
-    assertTrue("url '"+targetUrl+"' should exist",this.urlExists(targetUrl));
-	}
-	
+  /**
+   * test uploading a file
+   * 
+   * @throws Exception
+   */
+  @Test
+  public void testUpload() throws Exception {
+    ExampleWiki lwiki = ewm.get("mediawiki-japi-test1_24");
+    if (hasWikiUser(lwiki)) {
+      lwiki.login();
+      ClassLoader classLoader = getClass().getClassLoader();
+      File file = new File(classLoader
+          .getResource(
+              "upload/Radcliffe_Chastenay_-_Les_Mysteres_d_Udolphe_frontispice_T6.jpg")
+          .getFile());
+      String filename = file.getName();
+      String contents = "http://commons.wikimedia.org/wiki/File:Radcliffe_Chastenay_-_Les_Mysteres_d_Udolphe_frontispice_T6.jpg";
+      String reason = "test upload " + lwiki.wiki.getIsoTimeStamp();
+      lwiki.wiki.upload(file, filename, contents, reason);
+      String targetUrl = lwiki.wiki.getImageInfo("File:" + filename).getUrl();
+      assertTrue("url '" + targetUrl + "' should exist",
+          this.urlExists(targetUrl));
+    }
+  }
+
+  /**
+   * test uploading via image information
+   * 
+   * @throws Exception
+   */
+  @Test
+  public void testUploadViaII() throws Exception {
+    ExampleWiki sourceWiki = ewm.get("sourceWiki");
+    // sourceWiki.wiki.setDebug(true);
+    String imageName = "Index.png";
+    String imageTitle = "File:" + imageName;
+    Ii ii = sourceWiki.getImageInfo(imageTitle);
+    ExampleWiki targetWiki = ewm.get("targetWiki");
+    String pageContent = sourceWiki.wiki.getPageContent(imageTitle);
+    if (hasWikiUser(targetWiki)) {
+      targetWiki.login();
+      targetWiki.wiki.upload(ii, imageName, pageContent);
+      // targetWiki.wiki.setDebug(true);
+      String targetUrl = targetWiki.wiki.getImageInfo(imageTitle).getUrl();
+      assertTrue("url '" + targetUrl + "' should exist",
+          this.urlExists(targetUrl));
+    }
+  }
+
 }
