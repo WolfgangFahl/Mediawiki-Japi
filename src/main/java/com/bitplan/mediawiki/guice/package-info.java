538c4f7b4715a9f15f242daa701b53e3262ec806
Wolfgang Fahl <wf@bitplan.com>
2018-08-20 16:07:55 +0200

    fixes #40, #41

---
+++ src/main/java/com/bitplan/mediawiki/guice/package-info.java
@@ -0,0 +1,27 @@
+/**
+ *
+ * This file is part of the https://github.com/WolfgangFahl/Mediawiki-Japi open source project
+ *
+ * Copyright 2015-2018 BITPlan GmbH https://github.com/BITPlan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ *  You may obtain a copy of the License at
+ *
+ *  http:www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * supports dependency injection with guice to choose the implementation
+ * of MediaWiki you'd like to use - the BITPlan one or the one-class one 
+ * from MERC
+ */
+package com.bitplan.mediawiki.guice;
+ 
